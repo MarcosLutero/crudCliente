866de9cdd69dd769c036db346e3439fa65f66b8a
Marcos Lutero da Silva Rocha <lutero_rocha@outlook.com>
2021-06-03 07:53:28 -0300

    Project Created

---
+++ backend/src/test/java/com/empresa/crudcliente/CrudclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.crudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
