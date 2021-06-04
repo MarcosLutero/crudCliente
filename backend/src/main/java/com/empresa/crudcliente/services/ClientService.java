edb747d25fc2c2aed75ea836e6b449a6172eb18f
Marcos Lutero da Silva Rocha <lutero_rocha@outlook.com>
2021-06-04 16:49:43 -0300

    Search for id and Exception Created

--- backend/src/main/java/com/empresa/crudcliente/services/ClientService.java
+++ backend/src/main/java/com/empresa/crudcliente/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.empresa.crudcliente.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.empresa.crudcliente.dto.ClientDTO;
 import com.empresa.crudcliente.entities.Client;
 import com.empresa.crudcliente.repositories.ClientRepository;
+import com.empresa.crudcliente.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -26,6 +29,13 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
+		return new ClientDTO(entity);
 	}
 	
 }
